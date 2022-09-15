8c48c7232d9948cd644bc509681205f9c64ac9ff
Yehor Kikas <ekikas13@gmail.com>
2022-09-15 17:52:42 +0300

    10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,33 @@
+const boxesEl = document.getElementById("boxes");
+const createButtonEl = document.querySelector("[data-create]");
+const destroyButtonEl = document.querySelector("[data-destroy]");
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+};
+
+
+function createBoxes(amount) {
+  const newArr = [];
+  for (let i = 0; i < amount; i += 1) {
+    const newElement = document.createElement("div");
+    newElement.style.cssText = `width: ${30 + i * 10}px; height: ${30 + i * 10}px; background-color: ${getRandomHexColor()}`;
+    newArr.push(newElement);
+  }
+  return newArr;
+};
+
+function destroyBoxes() {
+  boxesEl.innerHTML = "";
+};
+
+
+createButtonEl.addEventListener("click", (event) => {
+  const amount = document.querySelector("#controls input").value;
+  destroyBoxes();
+  let newBoxesToAdd = createBoxes(amount);
+  boxesEl.append(...newBoxesToAdd);
+});
+
+destroyButtonEl.addEventListener("click", destroyBoxes);
